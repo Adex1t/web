67c95e5eaadf210b7270a236ef569f46f781df0a
holmanskih <evg.holmanskih@gmail.com>
2021-08-22 18:36:45 +0300

    add hometask to ls12

--- ls12/1-list/script.js
+++ ls12/1-list/script.js
@@ -34,3 +34,13 @@ for(let x = -10; x <= 10; x++) {
 
 
 let temp = [10, 15, 20, 15, 18, 18, 20, 10, 15, 20, 15, 18, 18, 20, 10, 15, 20, 15, 18, 18, 20, 10, 15, 20, 15, 18, 18, 20, 10, 15, 20, 15, 18, 18, 20, 10, 15, 20, 15, 18, 18, 20]
+// task 1
+// log all `temp` elements in range 5 12
+
+// task 2
+// in html create form with 2 input and 1 button 
+// first must contain `from` range number
+// second must contain `to` range number
+// on button click log all `temp` elements in range `from`, `to`
+
+// [from, to] = [5, 12] - все числа между 5, 12
